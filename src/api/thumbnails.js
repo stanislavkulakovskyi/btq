46e918548eb0a87090332bbaba3349b7c10672bd
kulakostas <92936150+kulakostas@users.noreply.github.com>
2023-06-21 17:56:59 +0300

    another one

--- src/api/thumbnails.js
+++ src/api/thumbnails.js
@@ -12,6 +12,9 @@ import thumb11 from '../assets/images/vid_thumbnails/11.webp';
 import thumb12 from '../assets/images/vid_thumbnails/12.webp';
 import thumb13 from '../assets/images/vid_thumbnails/13.webp';
 import thumb14 from '../assets/images/vid_thumbnails/14.webp';
+import thumb15 from '../assets/images/vid_thumbnails/15.webp';
+import thumb16 from '../assets/images/vid_thumbnails/16.webp';
+
 
 import microThumb1 from '../assets/images/vid_thumbnails/1_micro.webp';
 import microThumb2 from '../assets/images/vid_thumbnails/2_micro.webp';
@@ -29,41 +32,45 @@ import microThumb13 from '../assets/images/vid_thumbnails/13_micro.webp';
 import microThumb14 from '../assets/images/vid_thumbnails/14_micro.webp';
 
 const thumbs = [
-  {id: 1, image: thumb1, videoUrl: 'https://www.youtube.com/embed/6b1CfRII7wA?autoplay=1', microImage: microThumb1, roles: 'music + sfx'},
+  {id: 1, image: thumb1, videoUrl: 'https://www.youtube.com/embed/6b1CfRII7wA?autoplay=1', microImage: microThumb1, roles: 'music + sfx', name: 'aerotim'},
 
   {id: 2, image: thumb2, videoUrl: 'https://www.youtube.com/embed/gJjLlR-2CNI?autoplay=1', 
-  microImage: microThumb2, roles: 'music + sfx'},
+  microImage: microThumb2, roles: 'music + sfx', name: 'asak'},
 
   {id: 3, image: thumb3, videoUrl: 'https://www.youtube.com/embed/wvJo2_537F8?autoplay=1', 
-  microImage: microThumb3, roles: 'music + sfx'},
+  microImage: microThumb3, roles: 'music + sfx', name: 'bmw'},
 
   {id: 4, image: thumb4, videoUrl: 'https://www.youtube.com/embed/krkxCvpT7E0?autoplay=1', 
-  microImage: microThumb4, roles: 'music + sfx'},
+  microImage: microThumb4, roles: 'sfx', name: 'ccg'},
 
   {id: 5, image: thumb5, videoUrl: 'https://www.youtube.com/embed/JoRU3FNi15U?autoplay=1', 
-  microImage: microThumb5, roles: 'music + sfx'},
+  microImage: microThumb5, roles: 'music + sfx', name: 'moyo'},
 
   {id: 6, image: thumb6, videoUrl: 'https://www.youtube.com/embed/sjEjwjyUErY?autoplay=1', 
-  microImage: microThumb6, roles: 'music + sfx'},
+  microImage: microThumb6, roles: 'sfx', name: 'napapijri'},
 
   {id: 7, image: thumb7, videoUrl: 'https://www.youtube.com/embed/X600b-SzG80?autoplay=1', 
-  microImage: microThumb7, roles: 'music + sfx'},
+  microImage: microThumb7, roles: 'music + sfx', name: 'puma-dorn'},
 
   {id: 8, image: thumb8, videoUrl: 'https://www.youtube.com/embed/otWFdo5Bz_Y?autoplay=1', 
-  microImage: microThumb8, roles: 'music + sfx'},
+  microImage: microThumb8, roles: 'music + sfx', name: 'puma-tt'},
 
   {id: 9, image: thumb9, videoUrl: 'https://www.youtube.com/embed/2TsVwLv1Dm4?autoplay=1', 
-  microImage: microThumb9, roles: 'music + sfx'},
+  microImage: microThumb9, roles: 'sfx', name: 'shafa'},
 
-  {id: 10, image: thumb10, videoUrl: 'https://www.youtube.com/embed/46mndakk_8s?autoplay=1', microImage: microThumb10, roles: 'music + sfx'},
+  {id: 10, image: thumb10, videoUrl: 'https://www.youtube.com/embed/46mndakk_8s?autoplay=1', microImage: microThumb10, roles: 'sfx', name: 'smartass'},
 
-  {id: 11, image: thumb11, videoUrl: 'https://www.youtube.com/embed/hQCC3NPPXjk?autoplay=1', microImage: microThumb11, roles: 'music + sfx'},
+  {id: 11, image: thumb11, videoUrl: 'https://www.youtube.com/embed/hQCC3NPPXjk?autoplay=1', microImage: microThumb11, roles: 'music', name: 'snohetta'},
 
-  {id: 12, image: thumb12, videoUrl: 'https://www.youtube.com/embed/0l9tI1b5zS0?autoplay=1', microImage: microThumb12, roles: 'music + sfx'},
+  {id: 12, image: thumb12, videoUrl: 'https://www.youtube.com/embed/0l9tI1b5zS0?autoplay=1', microImage: microThumb12, roles: 'sfx', name: 'specialized-1'},
 
-  {id: 13, image: thumb13, videoUrl: 'https://www.youtube.com/embed/PLSOiOCyUjM?autoplay=1', microImage: microThumb13, roles: 'music + sfx'},
+  {id: 13, image: thumb13, videoUrl: 'https://www.youtube.com/embed/PLSOiOCyUjM?autoplay=1', microImage: microThumb13, roles: 'sfx', name: 'specialized-2'},
 
-  {id: 14, image: thumb14, videoUrl: 'https://www.youtube.com/embed/3kjgG_YHF90?autoplay=1', microImage: microThumb14, roles: 'music + sfx'},
+  {id: 14, image: thumb14, videoUrl: 'https://www.youtube.com/embed/3kjgG_YHF90?autoplay=1', microImage: microThumb14, roles: 'music + sfx', name: 'studdymo'},
+
+  {id: 15, image: thumb15, videoUrl: 'https://www.youtube.com/embed/B66TABG7GQg', roles: 'music + sfx', name: 'poltrona'},
+
+  {id: 16, image: thumb16, videoUrl: 'https://www.youtube.com/embed/oywZzXorGHs', roles: 'sfx', name: 'have-a-rest'}
 ];
 
 export default thumbs;
